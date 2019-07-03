988f55d51c25387df28bfea2168d25b7cb27ca70
Ved Prakash <reachvedpraksh@gmail.com>
2019-07-03 11:22:44 +0530

    Added Scoped Model to manage global state

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -3,10 +3,6 @@ import 'package:flutter/material.dart';
 import '../widgets/products/products.dart';
 
 class ProductsPage extends StatelessWidget {
-  final List<Map<String, dynamic>> products;
-
-  ProductsPage(this.products);
-
   Widget _buildSideDrawer(BuildContext context) {
     return Drawer(
       child: Column(
@@ -40,7 +36,7 @@ class ProductsPage extends StatelessWidget {
           )
         ],
       ),
-      body: Products(products),
+      body: Products(),
     );
   }
 }
